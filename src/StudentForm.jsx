1924bc41404156eedf06e85caa64fb0b79dacf5d
Brian Duong <brianduong@hotmail.com>
2024-04-01 13:06:05 -0400

    first commit

---
+++ src/StudentForm.jsx
@@ -0,0 +1,20 @@
+export default function StudentForm() {
+    return (
+        <div>
+            <form>
+            <div>
+                <label>Name:</label>
+                <input type="text" id="name" value="Brian D" readonly></input>
+            </div>
+            <div>
+                <label>Email:</label>
+                <input type="email" id="email"></input>
+            </div>
+            <div>
+                <label>Phone:</label>
+                <input type="text" id="phone"></input>
+            </div>
+            </form>
+        </div>
+    );
+};
